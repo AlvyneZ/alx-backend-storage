54278b99ead05822e23b97995045429408e1317d
AlvyneZ <alvynew@gmail.com>
2023-12-14 18:56:33 +0300

    Using 2022 instead of 2020

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- A script that lists the bands with 'Glam rock' as
 --  their main style, ranked by their longevity. metal-bands
 --  table is defined in 2-metal_bands.sql
-SELECT `band_name`, (IFNULL(`split`, 2020) - `formed`) AS `lifespan`
+SELECT `band_name`, (IFNULL(`split`, 2022) - `formed`) AS `lifespan`
     FROM `metal_bands`
     WHERE `style` LIKE '%Glam rock%'
     ORDER BY `lifespan` DESC;
