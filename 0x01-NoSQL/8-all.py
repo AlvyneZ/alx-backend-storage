35d9569dcd6b002534f887a4ef7ab01bbdc4e7aa
AlvyneZ <alvynew@gmail.com>
2023-12-30 18:25:59 +0300

    removing test code

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -15,13 +15,3 @@ def list_all(mongo_collection: Collection):
     Returns: A list of documents
     """
     return [doc for doc in mongo_collection.find()]
-
-
-if __name__ == "__main__":
-    from pymongo import MongoClient
-
-    client = MongoClient('mongodb://root:rootroot@127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
